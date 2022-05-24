59839ee711e02f22f024c5b53e73bbfdb8c765ab
KimYeaSeul <adgjl3151@hanmail.net>
2022-05-25 02:12:56 +0900

    actionListener 추가. hey, Is that all you can do?!

--- src/com/cal/Calculator.java
+++ src/com/cal/Calculator.java
@@ -25,6 +25,8 @@ public class Calculator extends JFrame {
 	private GridBagConstraints gbc = new GridBagConstraints();
 	TitledBorder tB = new TitledBorder(new LineBorder(Color.gray, 1));
 
+	String button_names[] = {"C", "±","%", "÷", "7", "8", "9", "x","4", "5", "6", "-", "1", "2", "3","+","0", ".", "="};
+	JButton buttons[] = new JButton[button_names.length];
 //	ButtonActionListener bal = new ButtonActionListener();
 	
 	public ArrayList<String> equation = new ArrayList<String>();
@@ -46,9 +48,6 @@ public class Calculator extends JFrame {
 		buttonPanel.setLayout(grid);
 		buttonPanel.setBounds(0, 70, width, 274);
 		
-		String button_names[] = {"C", "±","%", "÷", "7", "8", "9", "x","4", "5", "6", "-", "1", "2", "3","+","0", ".", "="};
-		String preference = "÷+=x-";
-		JButton buttons[] = new JButton[button_names.length];
 		
 		  //======
 			gbc.fill = GridBagConstraints.BOTH; // 꽉 채워줌
@@ -61,15 +60,16 @@ public class Calculator extends JFrame {
 			buttons[i] = new JButton(button_names[i]);
 			buttons[i].setFont(new Font("Arial", Font.BOLD, 20));
 			
-			if(preference.contains(button_names[i])) {
+//			if(preference.contains(button_names[i])) {
+			if(button_names[i].matches("[÷+=x-]")) {
 				buttons[i].setBackground(new Color(255, 159, 9));
 			}
 //			System.out.println("x : "+ x + "y : "+ y);
 			if(button_names[i] == "0") {
-				make(buttons[i], x, y, 2, 1);
+				makeFrame(buttons[i], x, y, 2, 1);
 				x++;
 			}else {
-				make(buttons[i], x, y, 1, 1);
+				makeFrame(buttons[i], x, y, 1, 1);
 			}
 			
 			x++;
@@ -78,7 +78,7 @@ public class Calculator extends JFrame {
 				y++;
 			}
 //			
-			buttons[i].addActionListener(new PadActionListener());
+			buttons[i].addActionListener(new ButtonActionListener());
 //			
 			buttons[i].setBorder(tB);
 			buttonPanel.add(buttons[i]);
@@ -97,7 +97,7 @@ public class Calculator extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫을때 실행중인 프로그램 도 종료
 	}
 
-	public void make(JButton c, int x , int y, int w, int h) {
+	public void makeFrame(JButton c, int x , int y, int w, int h) {
 		gbc.gridy = y;
 		gbc.gridx = x;
 		gbc.gridheight = h;
@@ -105,29 +105,67 @@ public class Calculator extends JFrame {
 		grid.setConstraints(c, gbc);
 	}
 	
-	class PadActionListener implements ActionListener{
+	// 연산자 중복입력 불가.
+	class ButtonActionListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) { // 이벤트 처리!
 			String operation = e.getActionCommand(); // 어떤 버튼이 눌렸나.
-			if(operation.equals("C")) {
+			// 취소키면 => 초기화
+			if(operation.equals("C")) { 
+				System.out.println("취소키");
 				inputSpace.setText("");
+			// 등호 => calculate함수에서 계산한 결과를 double형으로 반환해주고, inputSpace에 뿌려주고, num 초기화
 			}else if( operation.equals("=")) {
+				System.out.println("등호키");
 				String result = Double.toString(calculate(inputSpace.getText()));
 				inputSpace.setText(""+result);
 				num = "";
-			} else if(operation.contentEquals("+") || operation.contentEquals("-") || operation.contentEquals("x") || operation.contentEquals("÷")){
-				if(inputSpace.getText().equals("") && operation.equals("-")) {
-					inputSpace.setText(inputSpace.getText() + e.getActionCommand());
-				}else if(!inputSpace.getText().equals("") && !prev_operation.contentEquals("+") && !prev_operation.contentEquals("-") && !prev_operation.contentEquals("x") && !prev_operation.contentEquals("÷")) {
-					inputSpace.setText(inputSpace.getText() + e.getActionCommand());
+			 // 사칙연산이면!?
+			}else if(operation.equals("±")) {
+				System.out.println("음수플마키");
+				if(prev_operation.matches("[÷+x-]")  || prev_operation.matches("%") || inputSpace.getText().equals("") ) {
+					buttons[1].setBorder(new LineBorder(Color.red));
+//					inputSpace.setText(e.getActionCommand());
 				}
+				else 	prev_operation = String.valueOf(Integer.valueOf(prev_operation) * (-1)) ;
+				
+				// 하나로 합칠 방법이 있을까?
+			}else if(operation.matches("[÷+x-]") || operation.matches("%") ) {
+		    	// 
+		    	if(inputSpace.getText().equals("") ) {
+		    		inputSpace.setText("");
+		    		System.out.println("빈칸인데 사칙연산이 들어왔따");
+	    		}else if(prev_operation.matches("[÷+x-]")  || prev_operation.matches("%")  || prev_operation.matches("±")){
+		    	
+		    		System.out.println("전에 사칙연산인데 또 사칙연산이 들어왔다.)");
+		    		String is = inputSpace.getText();
+		    		String lastS = is.substring(0, is.length() -1 );
+		    		inputSpace.setText("");
+		    		inputSpace.setText(lastS + e.getActionCommand());
+		    		
+		    	}else {
+		    		System.out.println(" 숫자다음에 사칙연산이 들어왔다..");
+		    		inputSpace.setText(inputSpace.getText() + e.getActionCommand());
+		    		prev_operation = e.getActionCommand();
+		    	}
+				// 음수일때 -> 이건 따로 빼야지
+//				if(inputSpace.getText().equals("") && operation.equals("-")) { 
+//					inputSpace.setText(inputSpace.getText() + e.getActionCommand());
+				// 전에 사칙연산이 아니고, 빈칸이 아니라면 ( 숫자 다음에 사칙연산이 들어왔다면)
+//				 if(!inputSpace.getText().equals("") && !prev_operation.matches("[÷+x-]")) {
+					// 숫자 + 사칙연산 
+//				 }
+			// 숫자이면 => 연결연결
 			} else {
+				System.out.println("숫자인경우");
 				inputSpace.setText(inputSpace.getText() + e.getActionCommand());
 			}
+			// prev_operation에 현재 키 저장. => 다음 키와 비교위해
 			prev_operation = e.getActionCommand();
 		}
 	}
-	// 연산자 중복 입력 불가, 연산기호 우선순위
+	
+	// 연산기호 우선순위
 	private void fullTextParsing(String inputText) {
 		equation.clear();
 		for(int i=0; i<inputText.length(); i++) {
@@ -145,6 +183,7 @@ public class Calculator extends JFrame {
 		equation.remove("");
 	}
 	
+	// 1+2*3+2 이런식으로  String 이 들어오겠죠
 	public double calculate(String inputText) {
 		fullTextParsing(inputText);
 		
